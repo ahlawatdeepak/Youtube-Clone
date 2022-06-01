05e5c5524c23a4f405be45c13e228c11f3a81bc9
ahlawatdeepak <deepakahlawat735729@gmail.com>
2022-06-01 22:13:35 +0530

    change

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const api="AIzaSyCMtgIecnQb4p8CjBA3DcfGmy6ylt_zw6s"
+const api="AIzaSyB6Jrgi5iZKBxnItcRhn6i8lkKT4Tu_qvA"
 
-//  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=[YOUR_API_KEY]"
+//  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=AIzaSyB4jnjbEhkBUm6ojjiD470jJY-Di99xLpg"
 
 
 
+
+  // <<<<<<<<<<<<this is trending video url>>>>>>>>>>
+  let homeoutput=async() => {
+      try {
+        let url1=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&order=viewCount&key=${api}`
+        let res1=await fetch(url1)
+        let data1=await res1.json()
+        
+        //console.log(data1.items)
+          append(data1.items)
+      } catch (error) {
+          console.log(error)
+      } 
+  }
+
+  homeoutput()
+  
 // <<<<<<<<<<<<<search function onclick and async function>>>>>>>>>>
 // <<<<<<<<<<<<console the data>>>>>>>>>>>>>>>>>>>
     // <<<<<<<<<<<arrow function>>>>>>>>>>>>>>
+   
+
+
 let search= async () =>{
 
    try {
         let input=document.getElementById("input").value
-        let url=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&key=${api}`
+         let url=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&key=${api}`
+       
+      
 
         let res=await fetch(url)
 
